63014280a2d57d3fcec61400c647140b43c41d1d
DESKTOP-KLD3350\djdje <asdfg>
2018-05-01 12:03:05 +0900

    로그아웃

--- src/kr/co/bitbook/login/controller/LogoutController.java
+++ src/kr/co/bitbook/login/controller/LogoutController.java
@@ -17,7 +17,7 @@ public class LogoutController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		LoginMapper mapper = MyAppSqlConfig.getSqlSession().getMapper(LoginMapper.class);
-
+		//dd
 		request.getSession().invalidate();
 		response.sendRedirect(request.getContextPath() + "/bitbook/login");
 	}
